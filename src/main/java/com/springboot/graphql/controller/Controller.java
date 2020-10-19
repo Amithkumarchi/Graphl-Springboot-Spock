09d76e6d6221646f07b5ac9432709fa50ec75933
amith <amith.kumarchi@gmail.com>
2020-10-19 23:49:02 +0530

    calling from controller

--- src/main/java/com/springboot/graphql/controller/Controller.java
+++ src/main/java/com/springboot/graphql/controller/Controller.java
@@ -1,19 +1,38 @@
 package com.springboot.graphql.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.kickstart.execution.GraphQLQueryInvoker;
+import graphql.kickstart.execution.GraphQLQueryResult;
+import graphql.kickstart.execution.GraphQLRequest;
+import graphql.kickstart.execution.input.GraphQLSingleInvocationInput;
+import graphql.kickstart.servlet.input.GraphQLInvocationInputFactory;
+import graphql.schema.GraphQLSchema;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/books")
 @RestController
 public class Controller
 {
 
-    @PostMapping
-    public void getAllBooks(@RequestBody String query)
+    @Autowired
+    private GraphQLInvocationInputFactory invocationInputFactory;
+
+    @Autowired
+    private GraphQLQueryInvoker queryInvoker;
+
+    @PostMapping(value = "/posts")
+    public ResponseEntity<GraphQLQueryResult> getAllBooks(@RequestBody String query)
     {
 
+        GraphQLRequest request = GraphQLRequest.createQueryOnlyRequest(query);
+        GraphQLSingleInvocationInput invocationInput = invocationInputFactory.create(request);
+        GraphQLQueryResult result = queryInvoker.toGraphQLInvoker().query(invocationInput);
+       return new ResponseEntity<GraphQLQueryResult>(result,HttpStatus.OK);
     }
 
 
